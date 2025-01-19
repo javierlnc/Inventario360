1b6c2d892cc06244c71d00cfdc0e5f1739ec887f
javierlnc <javierlnc@icloud.com>
2025-01-18 21:24:11 -0500

    Refactor project structure to use 'api' as the main Django application and implement custom user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
 
 # Create your models here.
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = MyUserManager()
+
+    def __str__(self):
+        return self.email
