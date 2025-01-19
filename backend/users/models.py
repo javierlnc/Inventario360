d0c77cb624ba54956b72afea98615d4276da5a4b
javierlnc <javierlnc@icloud.com>
2025-01-18 21:27:55 -0500

    Implement custom user model and update settings for authentication

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,39 +3,6 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 
 # Create your models here.
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = self.model(
-            email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, date_of_birth, password=None):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-            date_of_birth=date_of_birth,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
-
-
 class MyUser(AbstractBaseUser):
     email = models.EmailField(unique=True, max_length=255)
     is_active = models.BooleanField(default=True)
@@ -44,7 +11,6 @@ class MyUser(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
-    objects = MyUserManager()
 
     def __str__(self):
         return self.email
